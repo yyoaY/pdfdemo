75575240c35db7c650b597aaad7675af4e7725c2
yyoay <844248010@qq.com>
2021-04-28 18:06:24 +0800

    手机端能展示

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,47 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<web-view class="pdf" :src="url"></web-view>
+		<!-- <image src="fg"></image> -->
+		<!-- <canvas style="width: 300px; height: 200px;" canvas-id="firstCanvas" id="firstCanvas"></canvas> -->
 		</view>
-	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
+export default {
+	data() {
+		return {
+			// pdf.js的viewer.htm所在路径
+			// 注意：静态的html文件需要放在根路径下的 hybrid/html 文件夹中
+			viewerUrl: '/hybrid/html/web/viewer.html',
+			// 要访问的pdf的路径
+			fileUrl: '/static/paper/fg.pdf',
+			// 最终显示在web-view中的路径
+			url: ''
+		};
+	},
+	onLoad() {
+		// h5，使用h5访问的时候记得跨域
+		// #ifdef H5
+		this.url = `${this.viewerUrl}?file=${encodeURIComponent(this.fileUrl)}`;
+		// #endif
+		
+		// 在安卓和ios手机上
+		// 判断是手机系统：安卓，使用pdf.js
+		// #ifdef APP-PLUS
+		if(plus.os.name === 'Android') {
+			this.url = `${this.viewerUrl}?file=${encodeURIComponent(this.fileUrl)}`;
 		}
-	}
+		// ios，直接访问pdf所在路径
+		else {
+			this.url = encodeURIComponent(this.fileUrl);
+		}
+		// #endif
+	},
+	methods: {}
+};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	.pdf{
 	}
 </style>
